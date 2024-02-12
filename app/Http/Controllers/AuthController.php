3cb4a71805125ca0bcdcc56aa765cc25053c1ecd
Mihaly Pirik <misipirik@gmail.com>
2024-02-12 11:32:29 +0100

    add login and register endpoints

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login() {
-        return "Logged in";
+    public function login(LoginUserRequest $request) {
+        $request->validated();
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(["message" => "Invalid credentials"], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $data = [
+            'user' => $user,
+            'token' => $user->createToken('API token')->plainTextToken,
+        ];
+
+        return response()->json($data, 200);
     }
 
     public function logout() {
         return "Logged out";
     }
 
-    public function register() {
-        return "Registered";
+    public function register(StoreUserRequest $request) {
+        $request->validated();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $data = [
+            'user' => $user,
+            'token' => $user->createToken('API token')->plainTextToken,
+        ];
+
+        return response()->json($data, 201);
+
     }
 }
